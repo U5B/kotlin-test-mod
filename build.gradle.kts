198582fde0db9cf63aa862d2baaa4a00af7f6430
U5B <56985400+U5B@users.noreply.github.com>
2023-04-19 19:31:31 -0700

    aaaaaaaaaaaaaaaaaaaa

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
-// reference: https://github.com/myoun/fabric-example-mod-kotlin-with-kotlin-dsl/blob/master/build.gradle.kts
-// 
+// Thanks to: https://github.com/myoun/fabric-example-mod-kotlin-with-kotlin-dsl/blob/master/build.gradle.kts
+// https://github.com/EssentialGG/essential-gradle-toolkit
 
 plugins {
-	id("fabric-loom") version "1.1-SNAPSHOT"
+	println("c!")
+	// id("fabric-loom") version "1.1-SNAPSHOT"
 	id("org.jetbrains.kotlin.jvm") version "1.8.20"
-	id("gg.essential.defaults.maven-publish") version "+"
-	id("gg.essential.defaults") version "+"
+	// id("gg.essential.defaults.maven-publish") version "+"
+	id("gg.essential.multi-version.root")
+	id("gg.essential.multi-version")
+	id("gg.essential.defaults")
 }
 
 val base_name: String by project
@@ -18,7 +21,6 @@ val fabric_kotlin_version: String by project
 val yarn_mappings: String by project
 val loader_version: String by project
 
-
 repositories {
 	// Add repositories to retrieve artifacts from in here.
 	// You should only use this when depending on other mods because
@@ -29,9 +31,9 @@ repositories {
 
 dependencies {
 	// To change the versions see the gradle.properties file
-	minecraft("com.mojang:minecraft:$minecraft_version")
-	mappings("net.fabricmc:yarn:$yarn_mappings:v2")
-	modImplementation("net.fabricmc:fabric-loader:$loader_version")
+	// minecraft("com.mojang:minecraft:$minecraft_version")
+	// mappings("net.fabricmc:yarn:$yarn_mappings:v2")
+	// modImplementation("net.fabricmc:fabric-loader:$loader_version")
 
 	// Fabric API. This is technically optional, but you probably want it anyway.
 	modImplementation("net.fabricmc.fabric-api:fabric-api:$fabric_version")
@@ -45,10 +47,9 @@ dependencies {
 	modImplementation("gg.essential:vigilance-$minecraft_version-fabric:284")
 	modImplementation("gg.essential:elementa-$minecraft_version-fabric:580")
 	modImplementation("gg.essential:universalcraft-$minecraft_version-fabric:262")
-
-	
 }
 
+/*
 tasks {
 	processResources {
 		inputs.property("version", mod_version)
@@ -83,11 +84,19 @@ tasks {
 				// mavenLocal()
 		}
 	}
+	*/
+}
+*/
+
+loom {
+
 }
 
+/*
 java {
 	// Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build" task
 	// if it is present.
 	// If you remove this line, sources will not be generated.
 	withSourcesJar()
 }
+*/
--- src/main/java/net/usbwire/mixin/ExampleMixin.java
+++ src/main/java/net/usbwire/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
 package net.usbwire.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
