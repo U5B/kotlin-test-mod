9ea9487ce196c450f4bf40a869f87d0bdb75473c
U5B <56985400+U5B@users.noreply.github.com>
2023-04-22 03:21:30 -0700

    making poi module (easiest)

--- src/main/kotlin/net/usbwire/base/features/Poi.kt
+++ src/main/kotlin/net/usbwire/base/features/Poi.kt
@@ -1,48 +1,109 @@
-package net.usbwire.base.features.Poi
+package net.usbwire.base.features
 
 import kotlinx.serialization.*
 import kotlinx.serialization.json.*
-
 import java.net.URL
-import java.io.File
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.nio.file.Files
 import java.nio.file.Path
-import java.io.FileOutputStream
+import gg.essential.api.EssentialAPI
+
 import net.usbwire.base.BaseMod
+import net.usbwire.base.util.Util
+import net.usbwire.base.config.VigilanceConfig
+import net.usbwire.base.command.PoiCommand
 
-@Serializable
-data class JsonPoi (
-  val name: String,
-  val shard: String,
-  val region: String?,
-  val subregion: String?,
-  val coordinates: JsonPoiCoordinates?
-)
-
-@Serializable
-data class JsonPoiCoordinates (
-  val x: Int,
-  val y: Int,
-  val z: Int
-)
-
-@kotlinx.serialization.ExperimentalSerializationApi
 class Poi {
-  fun fetchNewest (): Unit {
-    try {
-      URL("https://raw.githubusercontent.com/U5B/Monumenta/main/out/pois.json").openStream().use {
-        val project = Json.decodeFromStream<Map<String, JsonPoi>>(it) // read JSON from a URL
-        val iter = project.values.iterator()
-        while (iter.hasNext()) {
-          
-        }
-  
-        FileOutputStream(File("project.json")).use {  // and save to a file
-            Json.encodeToStream(project, it)
-        }
+  @Serializable
+  data class JsonPoi (
+    val name: String,
+    val shard: String,
+    val region: String?,
+    val subregion: String?,
+    val coordinates: JsonPoiCoordinates?
+  )
+
+  @Serializable
+  data class JsonPoiCoordinates (
+    val x: Int,
+    val y: Int,
+    val z: Int
+  )
+
+  val poiPath = Path.of("${BaseMod.configPath}/pois.json")
+  var poiMap: Map<String, JsonPoi> = emptyMap()
+  var poiSuggestions: Array<String> = emptyArray()
+
+  @kotlinx.serialization.ExperimentalSerializationApi
+  fun fetchPoiData () {
+    URL("https://raw.githubusercontent.com/U5B/Monumenta/main/out/pois.json").openStream().use {
+      val project = Json.decodeFromStream<Map<String, JsonPoi>>(it) // read JSON from a URL
+      poiMap = project
+      savePoiData()
+    }
+  }
+
+  @kotlinx.serialization.ExperimentalSerializationApi
+   fun loadPoiData () {
+    if (Files.notExists(poiPath)) return fetchPoiData() // don't use file if it doesn't exist
+    Files.newInputStream(poiPath).use {
+      val project = Json.decodeFromStream<Map<String, JsonPoi>>(it) // read JSON from a URL
+      poiMap = project
+    }
+  }
+
+  @kotlinx.serialization.ExperimentalSerializationApi
+  private fun savePoiData () {
+    if (poiMap.isEmpty()) return
+    Util.createPath(poiPath)
+    Files.newOutputStream(poiPath).use {
+      Json.encodeToStream(poiMap, it)
+    }
+  }
+
+  fun makeCommandSuggestions () {
+    val suggestions = ArrayList<String>()
+    poiMap.values.forEach { poi ->
+      suggestions.add(poi.name)
+    }
+    poiSuggestions = suggestions.toTypedArray()
+  }
+
+  fun searchPoi (input: String): ArrayList<JsonPoi>? {
+    // acutal logic
+    val response = ArrayList<JsonPoi>()
+    poiMap.forEach { poi ->
+      if (input == poi.value.name) {
+        response.add(poi.value)
+        return response
       }
+    }
+    poiMap.forEach { poi ->
+      val tags = Util.trimString(poi.value.name).split(' ')
+      if (tags.contains(Util.trimString(input))) response.add(poi.value)
+    }
+    if (response.size == 0) {
+      poiMap.forEach { poi ->
+        if (Util.cleanString(poi.value.name).contains(Util.cleanString(input))) response.add(poi.value)
+      }
+    }
+    if (response.size > 0) return response
+    return null
+  }
+
+  fun responsePoi (input: String, poi: JsonPoi) {
+    if (poi.coordinates != null) {
+      Util.chat("'${poi.name}': §a(${poi.coordinates.x}, ${poi.coordinates.y}, ${poi.coordinates.z})§r")
+    } else {
+      Util.chat("'${input}': No POI found.")
+    }
+  }
+
+  fun changeState (value: Boolean) {
+    if (value == true) {
+      loadPoiData()
+      PoiCommand.register()
+    } else if (value == false) {
+      EssentialAPI.getCommandRegistry().unregisterCommand(PoiCommand)
     }
   }
 }
