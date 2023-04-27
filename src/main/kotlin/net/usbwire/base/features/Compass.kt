1abb3d1bb8bb327738e006b655283d490c0c1b94
U5B <56985400+U5B@users.noreply.github.com>
2023-04-26 22:55:45 -0700

    make compass actually work! static methods no work

--- src/main/kotlin/net/usbwire/base/features/Compass.kt
+++ src/main/kotlin/net/usbwire/base/features/Compass.kt
@@ -2,9 +2,12 @@ package net.usbwire.base.features
 
 import gg.essential.universal.UMouse
 import gg.essential.universal.wrappers.UPlayer
+import net.minecraft.util.registry.Registry
+
 
 import net.usbwire.base.util.chat.Coordinates
 import net.usbwire.base.BaseMod
+import net.usbwire.base.util.Util
 
 object Compass {
   fun getCompass (): Coordinates.Coordinates {
@@ -12,14 +15,14 @@ object Compass {
     return Coordinates.Coordinates(spawnPos.x, spawnPos.y, spawnPos.z)
   }
 
-  fun createCompass (compass : Coordinates.Coordinates) {
+  fun createCompass (compass : Coordinates.Coordinates = getCompass()) {
     val dimension = BaseMod.mc.world!!.registryKey.value.toString()
     val name = "Compass"
     val x = compass.x
     val y = compass.y
     val z = compass.z
     val message = Coordinates.coordinateBuilder(name, x, y, z, dimension)
-    message.chat()
+    Util.chat(message)
   }
 
   var clicked = false;
@@ -27,7 +30,10 @@ object Compass {
   fun onTick () {
     val click = BaseMod.mc.mouse.wasLeftButtonClicked()
     if (click && clicked == false) {
-      BaseMod.logger.info(UPlayer.getPlayer()!!.mainHandStack.name.toString())
+      val mainItem = Registry.ITEM.getId(BaseMod.mc.player?.mainHandStack?.item).toString()
+      if (mainItem == "minecraft:compass") {
+        createCompass()
+      }
     }
     clicked = click
   }
